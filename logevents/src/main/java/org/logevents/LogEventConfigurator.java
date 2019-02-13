8d47060db9ccafff3099e2d79af686bc72a0be1f
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2019-02-13 23:16:24 +0100

    Fix Javadoc issues

--- logevents/src/main/java/org/logevents/LogEventConfigurator.java
+++ logevents/src/main/java/org/logevents/LogEventConfigurator.java
@@ -4,7 +4,7 @@ import java.util.ServiceLoader;
 
 /**
  * Implement this interface and specify your implementation class
- * in <tt>META-INF/services/org.logevents.LogEventConfigurator</tt> to
+ * in <code>META-INF/services/org.logevents.LogEventConfigurator</code> to
  * automatically load you own configuration with {@link ServiceLoader}
  * at startup. Your implementation may want to inherit from
  * {@link DefaultLogEventConfigurator}
