562d02d5d885fefbe8c9aea7dfd2846ee45f4947
Roy Sindre Norangshol <rono@unit.no>
2021-11-12 23:21:51 +0100

    Remove ContentType
    Removes coupling

--- logevents/src/main/java/org/logevents/util/ContentType.java
+++
@@ -1,7 +0,0 @@
-package org.logevents.util;
-
-public class ContentType {
-
-    public static final String APPLICATION_X_NDJSON = "application/x-ndjson";
-    public static final String APPLICATION_JSON = "application/json";
-}
