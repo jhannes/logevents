cde2db2eef1175a2aa99ae23836ffb66f9a2f92e
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-05-04 23:22:48 +0200

    LogEventStatus merges head and tail messages together for queries (documentation)

--- logevents/src/main/java/org/logevents/status/package-info.java
+++ logevents/src/main/java/org/logevents/status/package-info.java
@@ -1,8 +1,8 @@
 /**
  * Reporting of internal events such as configuration loading and errors from
  * logevents. {@link org.logevents.status.StatusEvent}s are collected in
- * {@link org.logevents.status.LogEventStatus} and can be retrieved with
- * {@link org.logevents.status.LogEventStatus#getMessages}
+ * {@link org.logevents.status.LogEventStatus} and can be stored in
+ * {@link org.logevents.status.LogEventStatus#getHeadMessages}
  * (which stores the first 1000 messages) and
  * {@link org.logevents.status.LogEventStatus#lastMessage}.
  * In order to debug LogEvents, either put <code>status=[DEBUG|TRACE|CONFIG]</code>
